577adc2f15a653465adc3e9e6c711c13eb2a7607
msVill <msv.ltmc@gmail.com>
2026-03-12 19:14:29 -0700

    Updated:the dynamic chair object to use the proper cosntructor and tje dynamic array of chair objects so thhat it now just creates the objects and initializes them using a for-loop only

--- 210-lab-20.cpp
+++ 210-lab-20.cpp
@@ -71,6 +71,7 @@ int main() {
     chairPtr->print();
 
     //creating dynamic chair object with constructor
+    double prices[SIZE] = {525.25, 434.34, 252.52};
     Chair *livingChair = new Chair(3, prices);
     livingChair->print();
     delete livingChair;
@@ -78,12 +79,7 @@ int main() {
 
     //creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
+
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
     
